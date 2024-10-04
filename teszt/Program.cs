20614e40b9ab0d9e590f983151855ec0eed705e7
2022103 <2022103@B104-13>
2024-10-04 14:11:49 +0200

    Add project files.

---
+++ teszt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teszt
+{
+    class ElsőProgram
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
